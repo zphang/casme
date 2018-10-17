ffc28c646396362d2cedb5f81c51fdef61f4bf0b
Jason Phang <email@jasonphang.com>
2018-10-17 01:01:10 -0400

    restore_plot

--- archs.py
+++ archs.py
@@ -208,4 +208,4 @@ class Masker(nn.Module):
         return new_l
 
 def decoder(**kwargs):
-    return Masker([64, 256, 512, 1024, 2048], **kwargs)
+    return Masker([64, 256, 512, 1024, 2048], 64 **kwargs)
